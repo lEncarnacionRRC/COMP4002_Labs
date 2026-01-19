f19a3d4059710e1da3b462e6eafee770adfe07ff
Lindsay Encarnacion <lencarnacion@academic.rrc.ca>
2026-01-18 23:49:03 -0600

    created react project with vite and making new files with each component along with type safety

---
+++ fs_lab-1.2/src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Employee Directory</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ fs_lab-1.2/src/components/Main.tsx
@@ -0,0 +1,25 @@
+import { Department } from '../types/Employee'
+import employeeData from '../data/employees.json'
+
+function Main() {
+  const departments: Department[] = employeeData
+
+  return (
+    <main id="employee-list">
+      {departments.map((department) => (
+        <div key={department.departmentName}>
+          <h3>{department.departmentName}</h3>
+          <ul>
+            {department.employees.map((emp, index) => (
+              <li key={`${emp.firstName}-${emp.lastName}-${index}`}>
+                {emp.firstName} {emp.lastName}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </main>
+  )
+}
+
+export default Main
